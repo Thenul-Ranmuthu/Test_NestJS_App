ccc06d6999528fb2e0559f033d0f052adb621958
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-07-20 18:01:58 +0530

    feat(auth): add HTTP status code for login endpoint in CatsAuthController

--- src/cats/Controllers/cats.auth.controller.ts
+++ src/cats/Controllers/cats.auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Logger, Post } from '@nestjs/common';
 import { CatsAuthService } from '../Services/cats.auth.service';
 import { CatsLoginDto } from '../Dtos/catsLogin.dto';
 import { CatsLoginResponseDto } from '../Dtos/catsLoginResponse.dto';
@@ -11,6 +11,7 @@ export class CatsAuthController {
   private readonly logger = new Logger(CatsAuthController.name);
 
   @Post('login')
+  @HttpCode(200)
   async login(@Body() loginDto: CatsLoginDto): Promise<CatsLoginResponseDto> {
     this.logger.log(`Received: ${JSON.stringify(loginDto)} (for LOGIN)`);
     return this.catAuthService.login(
