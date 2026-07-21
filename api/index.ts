758516ba21621a6d1e9f6d51edc20233a081176e
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-07-21 15:09:33 +0530

    fix: correct formatting and add missing semicolons in index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,20 +4,20 @@ import express from 'express';
 import { AppModule } from 'src/app.module';
 
 const server = express();
-let isBootstraped = false
+let isBootstraped = false;
 
-async function bootstrap(){
-    if(!isBootstraped){
-        const app =await NestFactory.create(AppModule, new ExpressAdapter(server))
-    app.enableCors()
-    await app.init()
-    }
-    return server
+async function bootstrap() {
+  if (!isBootstraped) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.enableCors();
+    await app.init();
+  }
+  return server;
 }
 
-bootstrap()
+bootstrap();
 
-export default async (req: any, res: any) =>{
-    const server = await bootstrap()
-    server(req,res)
-}
+export default async (req: any, res: any) => {
+  const server = await bootstrap();
+  server(req, res);
+};
